d5bc4a5a0a9844a25c0f2d2cd3e0e8fc1a7381ea
AlGartner <alvino.gartner@gmail.com>
2022-02-12 23:58:24 +0700

    push lagi

--- wpu-movie/js/script.js
+++ wpu-movie/js/script.js
@@ -8,6 +8,7 @@ $('#search-button').on('click', function(){
         },
         dataType: "json",
         success: function (data) {
+            console.log(data);
            if (data.Response == "True"){
                 let movies = data.Search;
 
@@ -16,7 +17,7 @@ $('#search-button').on('click', function(){
                         <div class="card" >
                             <img src="..." class="card-img-top" alt="...">
                             <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
+                            <h5 class="card-title"></h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
